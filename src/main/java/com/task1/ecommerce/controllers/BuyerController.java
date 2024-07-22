f82bb2abe2e5aa3ff159c2902243dc7a29b3793f
Agboola Tobi Samuel <tobi4tee@gmail.com>
2024-07-22 15:38:23 +0100

    Postman Testing and refractoring of the method that adds product to cart...

--- src/main/java/com/task1/ecommerce/controllers/BuyerController.java
+++ src/main/java/com/task1/ecommerce/controllers/BuyerController.java
@@ -5,9 +5,11 @@ import com.task1.ecommerce.data.models.Buyer;
 import com.task1.ecommerce.dtos.requests.AddToCartRequest;
 import com.task1.ecommerce.dtos.requests.BuyerRegistrationRequest;
 import com.task1.ecommerce.dtos.requests.OrderRequest;
+import com.task1.ecommerce.dtos.requests.RemoveProductFromCartRequest;
 import com.task1.ecommerce.dtos.responses.AddToCartResponse;
 import com.task1.ecommerce.dtos.responses.BuyerRegistrationResponse;
 import com.task1.ecommerce.dtos.responses.OrderResponse;
+import com.task1.ecommerce.dtos.responses.RemoveProductFromCartResponse;
 import com.task1.ecommerce.exceptions.*;
 import com.task1.ecommerce.services.BuyerService;
 import lombok.AllArgsConstructor;
@@ -30,6 +32,11 @@ public class BuyerController {
     @PostMapping("addProductToCart")
     public ResponseEntity<AddToCartResponse> addToCart(@RequestBody AddToCartRequest request) throws BuyerNotFoundException, ProductNotFoundException {
         return new ResponseEntity<>(buyerService.addProductToCart(request), HttpStatus.CREATED);
+    }
+
+    @PostMapping("removeItemFromCart")
+    public ResponseEntity<RemoveProductFromCartResponse> removeFromCart(@RequestBody RemoveProductFromCartRequest request) throws BuyerNotFoundException, ProductNotFoundException, CartItemException {
+        return new ResponseEntity<>(buyerService.removeProductFromCart(request),HttpStatus.OK);
     }
 
     @PostMapping("makeOrder")
--- src/main/java/com/task1/ecommerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/task1/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -10,58 +10,71 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SellerRegistrationException.class)
-    public ResponseEntity<String> serviceFusionException(SellerRegistrationException exception){
+    public ResponseEntity<String> sellerRegistrationException(SellerRegistrationException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(BuyerExistException.class)
-    public ResponseEntity<String> serviceFusionException(BuyerExistException exception){
+    public ResponseEntity<String> buyerRegistrationException(BuyerExistException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BuyerNotFoundException.class)
-    public ResponseEntity<String> serviceFusionException(BuyerNotFoundException exception){
+    public ResponseEntity<String> buyerNotFoundException(BuyerNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BuyerRegistrationException.class)
-    public ResponseEntity<String> serviceFusionException(BuyerRegistrationException exception){
+    public ResponseEntity<String> buyerRegistrationException(BuyerRegistrationException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(CartItemException.class)
+    public ResponseEntity<String> cartItemException(CartItemException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+
     @ExceptionHandler(EmptyCartException.class)
-    public ResponseEntity<String> serviceFusionException(EmptyCartException exception){
+    public ResponseEntity<String> emptyCartException(EmptyCartException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ExistingProductException.class)
-    public ResponseEntity<String> serviceFusionException(ExistingProductException exception){
+    public ResponseEntity<String> existingProductException(ExistingProductException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<String> invalidCredentialsException(InvalidCredentialsException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidPhoneNumberException.class)
-    public ResponseEntity<String> serviceFusionException(InvalidPhoneNumberException exception){
+    public ResponseEntity<String> invalidPhoneNumberException(InvalidPhoneNumberException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
 
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
-    public ResponseEntity<String> serviceFusionException(OrderNotFoundException exception){
+    public ResponseEntity<String> orderNotFoundException(OrderNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<String> serviceFusionException(ProductNotFoundException exception){
+    public ResponseEntity<String> productNotFoundException(ProductNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(SellerNotFoundException.class)
-    public ResponseEntity<String> serviceFusionException(SellerNotFoundException exception){
+    public ResponseEntity<String> sellerNotFoundException(SellerNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(StoreNotFoundException.class)
-    public ResponseEntity<String> serviceFusionException(StoreNotFoundException exception){
+    public ResponseEntity<String> toreNotFoundException(StoreNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
 
     }
+
 }
